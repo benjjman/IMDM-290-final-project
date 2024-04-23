904dd852e02b0b436617e12c3e6e8991a50bda89
benjjman <benjjman@umd.edu>
2024-04-23 10:18:14 -0400

    bruh
    trying to put everything together

--- IMDM-290-final/Assets/Scripts/ParticleRippleController.cs
+++ IMDM-290-final/Assets/Scripts/ParticleRippleController.cs
@@ -8,12 +8,24 @@ public class ParticleRippleController : MonoBehaviour
     public ParticleSystem ripple;
     public GameObject player;   //to reference player's position 
     public GameObject hit; //the star or planet or wtv that you hit
+    //public GameObject collisionPoint;
 
 
     public void Update() //OnTriggerEnter()   //when star is hit 
     {
+<<<<<<< Updated upstream
         if (Input.anyKey){
             
+=======
+    }
+
+    private void OnTriggerEnter()//(Collider collide)   //when star is hit 
+    {       
+
+            //collisionPoint.transform.position = collide.transform.position; 
+            //var collision = Collider.ClosestPoint(collisionPoint.transform.position);
+         
+>>>>>>> Stashed changes
             //initialize rotation/position perpendicular to the spot that was shot
             ripple.transform.position = hit.transform.position;
             var direction = player.transform.position - ripple.transform.position;
@@ -22,7 +34,11 @@ public class ParticleRippleController : MonoBehaviour
             //rotation.z = rotation.z * -1;
             ripple.transform.rotation = rotation; 
             ripple.Play();
+<<<<<<< Updated upstream
         }
+=======
+
+>>>>>>> Stashed changes
     }
 }
 
