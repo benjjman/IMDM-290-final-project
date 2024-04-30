d7f462446a4a941f14fba5fa9c4460b6abd52568
benjjman <benjjman@umd.edu>
2024-04-30 05:45:34 -0400

    projectile positioning and initialization
    particleRippleController mainly, also some small additions to DeleteTimer(lines 9&10, 18-21)

--- IMDM-290-final/Assets/Scripts/DeleteTimer.cs
+++ IMDM-290-final/Assets/Scripts/DeleteTimer.cs
@@ -6,6 +6,8 @@ public class DeleteTimer : MonoBehaviour
 {
 
     public float aliveTime = 5f;
+    public bool end = false; 
+    public float buffer = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,10 @@ public class DeleteTimer : MonoBehaviour
     }
 
     private IEnumerator WaitThenDelete(){
-        yield return new WaitForSeconds(aliveTime);
+        yield return new WaitForSeconds(aliveTime - buffer);
+        end = true; 
+        yield return new WaitForSeconds(buffer);
+        end = false; 
         Destroy(transform.gameObject);
     }
 
--- IMDM-290-final/Assets/Scripts/ParticleRippleController.cs
+++ IMDM-290-final/Assets/Scripts/ParticleRippleController.cs
@@ -2,20 +2,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.FilePathAttribute;
 
 public class ParticleRippleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ParticleSystem ripple;
+
+    void Start()    
+    {
+        ripple.Stop();
+    }
+
+    void Update()   
+    {
+        GameObject wandshot = GameObject.FindWithTag("WandParticle");
+        bool over = wandshot.GetComponent<DeleteTimer>().end; 
+        if (over)  
+        {
+            StartCoroutine(CreateWaitDelete(wandshot, ripple));
+        }
+    }
+
+    private IEnumerator CreateWaitDelete(GameObject hit, ParticleSystem ripple)  //called after wand particle dissipates (when it hits target hopefully)
+    {
+        ParticleSystem rippleClone = ripple;
+        RippleReposition(hit, rippleClone);
+        rippleClone.Play();
+        yield return new WaitForSeconds(rippleClone.duration + 0.5f);
+        //rippleClone.Stop();
+    }
+
+    private void RippleReposition(GameObject hit, ParticleSystem ripple)   
     {
         //initialize rotation/position perpendicular to the spot that was shot
+        ripple.transform.position = hit.transform.position;
+        var playerPos = new Vector3(0,0,0);
+        var direction = playerPos - ripple.transform.position;
+        Quaternion rotation = Quaternion.LookRotation(playerPos, direction);
+        //rotation.z = rotation.z * -1;
+        ripple.transform.rotation = rotation;
     }
 
-    //start at impact and stop at ~ 8-9 s (fade out for like ~1 sec so it's not abrupt)
 
-    // Update is called once per frame
-    void Update()
-    {
-        //? anything else? 
-    }
+    //ig this is so that the ripple only plays if u hit a target / under the same conditions as a sound being triggered
+    //private void OnTriggerEnter()   //~from SoundTrigger -- add script to same object as SoundTrigger (??)
+    //{
+
+    //    //collisionPoint.transform.position = collide.transform.position; 
+    //    //var collision = Collider.ClosestPoint(collisionPoint.transform.position);
+
+    //    //initialize rotation/position perpendicular to the spot that was shot
+    //    ripple.transform.position = hit.transform.position;
+    //    var direction = player.transform.position - ripple.transform.position;
+    //    rotation.z = rotation.z * -1;
+    //    ripple.transform.rotation = rotation;
+    //    ripple.Play();
+    //}
 }
+
